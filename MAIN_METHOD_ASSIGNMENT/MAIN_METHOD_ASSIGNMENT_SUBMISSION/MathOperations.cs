ddc936c781a32112f0c4a05a1ccf0a23bcaa2aa3
Viktoriya (Vik) <127991377+SassyCatSlaps@users.noreply.github.com>
2023-05-12 10:21:56 -0600

    MAIN METHOD SUBMISSION ASSIGNMENT
    performing random maths  with user input in simple console app

---
+++ MAIN_METHOD_ASSIGNMENT/MAIN_METHOD_ASSIGNMENT_SUBMISSION/MathOperations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIN_METHOD_ASSIGNMENT_SUBMISSION
+{
+    public class MathOperations
+    {
+        // Create a class to encapsulate the math operations
+        public class Calculator
+        {
+            // Method that takes an integer, performs a math operation, and returns an integer
+            public int PerformMathOperation(int number)
+            {
+                // Add 10 to the input number and return the result
+                return number + 10;
+            }
+
+            // Overloaded method that takes a decimal, performs a math operation, then returns an integer
+            public int PerformMathOperation(decimal number)
+            {
+                // Multiply the input number by 2 & return result as an integer
+                return (int)(number * 2);
+            }
+
+            // Overloaded method that takes a string, converts it to an integer, performs a math operation, & returns an integer
+            public int PerformMathOperation(string numberString)
+            {
+                // Try to convert the input string to an integer
+                if (int.TryParse(numberString, out int number))
+                {
+                    // Square the input number and return the result
+                    return number * number;
+                }
+                else
+                {
+                    // If input string is not a valid integer, return 0
+                    return 0;
+                }
+            }
+        }
+    }
+}
---
+++ MAIN_METHOD_ASSIGNMENT/MAIN_METHOD_ASSIGNMENT_SUBMISSION/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIN_METHOD_ASSIGNMENT_SUBMISSION
+
+//MAIN METHOD SUBMISSION ASSIGNMENT
+
+// Perform these actions and create a console app that includes the following:
+// Create a class.
+// In that class, create a method that will take in an integer, create a math operation for this integer (addition, subtraction, etc.),
+// then return the answer as an integer.
+// In the Main() method of the console app, instantiate the class and call the one method, passing in an integer.
+// Display the result to the screen.
+// Add a second method to the class with the same name that will take in a decimal, create a different math operation for it, then return the answer as an integer.
+// In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal.
+// Display the result to the screen.
+// Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it, then return the answer as an integer.
+// In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer.
+// Display the result to the screen.
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the Calculator class
+            MathOperations.Calculator calculator = new MathOperations.Calculator();
+
+            // Prompt user to enter an integer
+            Console.Write("Enter an integer: ");
+
+            // Read user input as a string
+            string userInput = Console.ReadLine();
+
+            // Convert user input to an integer
+            int NUM = int.Parse(userInput);
+
+            // Call PerformMathOperation method with the integer input & display result
+            Console.WriteLine("Result 1: " + calculator.PerformMathOperation(NUM));
+
+            // Prompt user to enter a decimal
+            Console.Write("Enter a decimal: ");
+
+            // Read user input as a string
+            userInput = Console.ReadLine();
+
+            // Convert user input to a decimal
+            decimal decimalNumber = decimal.Parse(userInput);
+
+            // Call the PerformMathOperation method with the decimal input and display the result
+            Console.WriteLine("Result 2: " + calculator.PerformMathOperation(decimalNumber));
+
+            // Prompt user to enter a string that represents an integer
+            Console.Write("Enter a string representing an integer: ");
+
+            // Read user input as a string
+            userInput = Console.ReadLine();
+
+            // Call the PerformMathOperation method with the string input and display result
+            Console.WriteLine("Result 3: " + calculator.PerformMathOperation(userInput));
+
+            // Give user chance to read output before closing console window
+            Console.ReadLine();
+        }
+    }
+}
