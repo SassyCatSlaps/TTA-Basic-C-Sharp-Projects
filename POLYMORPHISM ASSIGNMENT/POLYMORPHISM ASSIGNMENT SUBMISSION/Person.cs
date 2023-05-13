9987d13ef029870fa1bd8d63b239b90957280ae8
Viktoriya (Vik) <127991377+SassyCatSlaps@users.noreply.github.com>
2023-05-13 01:04:12 -0600

    POLYMORPHISM ASSIGNMENT
    Uses code in previous assignment ' ABSTRACT_CLASS_SUBMISSION_ASSIGNMENT' to demonstrate polymorphism, create an interface, and obj type IQuittable

---
+++ POLYMORPHISM ASSIGNMENT/POLYMORPHISM ASSIGNMENT SUBMISSION/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POLYMORPHISM_ASSIGNMENT_SUBMISSION
+{
+    // Code from previous 'ABSTRACT_CLASS_SUBMISSION_ASSIGNMENT'
+    // Create an abstract class called Person with two properties: string firstName and string lastName
+    public abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        // Give it the method SayName().
+        public abstract void SayName();
+    }
+
+    // NEW CODE:
+    // Create another class called Employee and have it inherit from the Person class
+    public class Employee : Person, IQuittable
+    {
+        // Implement the SayName() method inside of the Employee class
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: {firstName} {lastName}");
+        }
+
+        // NEW CODE:
+        // Implement the Quit() method from the IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine("Quit your job!");
+        }
+    }
+}
+
---
+++ POLYMORPHISM ASSIGNMENT/POLYMORPHISM ASSIGNMENT SUBMISSION/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// POLYMORPHISM ASSIGNMENT
+
+// Perform these actions and create a console app that includes the following:
+// Create an interface called IQuittable and have it define a void method called Quit().
+// Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+// Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
+// Hint: an object can be of an interface type if it implements that specific interface.
+// Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
+
+namespace POLYMORPHISM_ASSIGNMENT_SUBMISSION
+{
+    // NEW CODE:
+    // Create an interface called IQuittable & have it define a void method called Quit()
+    interface IQuittable
+    {
+        void Quit();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code from previous 'ABSTRACT_CLASS_SUBMISSION_ASSIGNMENT'
+            // Inside the Main() method instantiate an Employee object w/ firstName “Sample” and lastName “Student”
+            Employee employee = new Employee
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+            // Call the SayName() method on the object
+            employee.SayName();
+
+            // NEW CODE:
+            // Use polymorphism to create an object of type IQuittable and call the Quit() method on it
+            IQuittable quittableEmployee = new Employee();
+
+            // Call the Quit() method on the IQuittable object
+            quittableEmployee.Quit();
+
+            // Moment to read console output -- ' Name: Sample Student \n Quit your job! '
+            Console.ReadLine();
+        }
+    }
+}
