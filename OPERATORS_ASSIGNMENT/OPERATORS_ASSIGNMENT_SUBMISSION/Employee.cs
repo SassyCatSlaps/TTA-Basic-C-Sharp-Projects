a5e27cf4f1c67208a4a76ea771e19197d8751492
Viktoriya (Vik) <127991377+SassyCatSlaps@users.noreply.github.com>
2023-05-15 17:56:19 -0600

    OPERATORS ASSIGNMENT SUBMISSION
    Showing the use of overload with operators and doing comparisons

---
+++ OPERATORS_ASSIGNMENT/OPERATORS_ASSIGNMENT_SUBMISSION/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERATORS_ASSIGNMENT_SUBMISSION
+{
+    // Create an Employee class with Id, FirstName and LastName properties  --  I put FirstName and LastName into a 'Person' class and had the Employee class inherit it
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        // In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property
+        public static bool operator== (Employee employee01, Employee employee02)
+        {
+            if (employee01.Id == employee02.Id)
+                return true;
+            else
+            return employee01.Id == employee02.Id;
+        }
+        // Comparison operators must be overloaded in pairs
+        public static bool operator!= (Employee employee01, Employee employee02)
+        {
+            return employee01.Id != employee02.Id;
+        }
+        public override bool Equals(object obj)
+        {
+            var emp = obj as Employee;
+            if (emp == null)
+                return false;
+            return Id.Equals(emp.Id);
+        }
+    }
+}
---
+++ OPERATORS_ASSIGNMENT/OPERATORS_ASSIGNMENT_SUBMISSION/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Perform these actions and create a console app that includes the following:
+// Create an Employee class with Id, FirstName and LastName properties. 
+// In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property.
+// Remember that comparison operators must be overloaded in pairs.
+// In the "Program.cs" file, instantiate two objects of the Employee class and assign values to their properties.
+// Then compare the two Employee objects using the newly overloaded operators and display the results.
+// Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
+
+namespace OPERATORS_ASSIGNMENT_SUBMISSION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In the "Program.cs" file, instantiate two objects of the Employee class and assign values to their properties
+            Employee employee01 = new Employee();
+            employee01.FirstName = "Vikko";
+            employee01.LastName = "Vibes";
+            employee01.Id = 0006;
+
+            Employee employee02 = new Employee();
+            employee02.FirstName = "Kharon";
+            employee02.LastName = "Ferryman";
+            employee02.Id = 666666;
+
+            // Then compare the two Employee objects using the newly overloaded operators and display the results
+            if (employee01 == employee02)
+            {
+                Console.WriteLine("These two employees have identical ID numbers.");
+            }
+            else
+            {
+                Console.WriteLine("These two employees have different ID numbers.");
+            }
+            // Moment for user to read output before closing program
+            Console.ReadLine();
+        }
+    }
+}
