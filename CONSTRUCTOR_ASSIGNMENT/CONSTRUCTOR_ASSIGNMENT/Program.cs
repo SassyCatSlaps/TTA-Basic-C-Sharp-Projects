2c5b371e86d080960e29f4d348e536ef9dc39870
Viktoriya (Vik) <127991377+SassyCatSlaps@users.noreply.github.com>
2023-05-22 10:35:21 -0600

    Constructor Assignment
    This demonstrates the use of the constructor and variable keywords to assign values to multiple variables

---
+++ CONSTRUCTOR_ASSIGNMENT/CONSTRUCTOR_ASSIGNMENT/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Perform these actions and create a console app that includes the following:
+// Create a const variable.
+// Create a variable using the keyword “var.”
+// Chain two constructors together.
+// Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
+
+namespace CONSTRUCTOR_ASSIGNMENT
+{
+    class Program
+    {
+        // Create a const variable
+        const int vikConstant = 199;
+        static void Main(string[] args)
+        {
+            // Create a variable using the keyword “var.”
+            var vikVariable = "Hallo zussammen!";
+
+            // Chain two constructors together
+            vikClass vikObj = new vikClass("A modern ape");
+
+            // Test print these creations to console
+            Console.WriteLine("The constant value: " + vikConstant);
+            Console.WriteLine("The variable value: " + vikVariable);
+            Console.WriteLine("The object name: " + vikObj.Bezeichnung);
+
+            // Give user chance to read output
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CONSTRUCTOR_ASSIGNMENT/CONSTRUCTOR_ASSIGNMENT/vikClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSTRUCTOR_ASSIGNMENT
+{
+    public class vikClass
+    {
+        public string Bezeichnung { get; set; }
+
+        // Parameter for a constructor
+        public vikClass(string name)
+        {
+            Bezeichnung = name;
+        }
+        // Connecting/chaining constructors w/ eachother
+        public vikClass() : this("Voreinstellung")
+        {
+        }
+    }
+}
